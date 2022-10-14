6fad1bfb0f528e74ab6a9d25ed817b9cfc462324
DmitriyKnol <Dmi3ydik@yandex.ru>
2022-10-14 20:05:58 +0500

    add a test

--- src/main/java/ya/praktikum/veb/Main.java
+++ src/main/java/ya/praktikum/veb/Main.java
@@ -1,10 +1,9 @@
 package ya.praktikum.veb;
 
-import javax.sound.midi.Soundbank;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello me to");
+        assert 2 * 3 == 5;
     }
 }
