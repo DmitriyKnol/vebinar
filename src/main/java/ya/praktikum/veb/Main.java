0b02b18cc5cc65c4650b479336e7464e9f79b5f6
DmitriyKnol <Dmi3ydik@yandex.ru>
2022-10-14 12:43:11 +0500

    add maven project

---
+++ src/main/java/ya/praktikum/veb/Main.java
@@ -0,0 +1,10 @@
+package ya.praktikum.veb;
+
+import javax.sound.midi.Soundbank;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello me to");
+    }
+}
